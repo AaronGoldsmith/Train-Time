8b9cc4d78fa93a1b2c33f943c70fcabeb49cce7e
Aaron Goldsmith <agoldsmith@scu.edu>
2018-08-17 01:47:01 -0700

    still figuring out the push op

--- assets/script.js
+++ assets/script.js
@@ -14,6 +14,7 @@
 
 // db ref
 var database = firebase.database();
+var rootRef = database.ref("/")
 var roleList = ["Team Lead","Project Manager", "Developer", "Sales", "UI/UX"];
 // var cities = ["Albuquerque","Alexandria","Allentown","Amarillo","Anaheim","Anchorage","Ann Arbor","Antioch","Apple Valley","Appleton","Arlington","Arvada","Asheville","Athens","Atlanta","Atlantic City","Augusta","Aurora","Austin","Bakersfield","Baltimore","Barnstable","Baton Rouge","Beaumont","Bel Air","Bellevue","Berkeley","Bethlehem","Billings","Birmingham","Bloomington","Boise","Boise City","Bonita Springs","Boston","Boulder","Bradenton","Bremerton","Bridgeport","Brighton","Brownsville","Bryan","Buffalo","Burbank","Burlington","Cambridge","Canton","Cape Coral","Carrollton","Cary","Cathedral City","Cedar Rapids","Champaign","Chandler","Charleston","Charlotte","Chattanooga","Chesapeake","Chicago","Chula Vista","Cincinnati","Clarke County","Clarksville","Clearwater","Cleveland","College Station","Colorado Springs","Columbia","Columbus","Concord","Coral Springs","Corona","Corpus Christi","Costa Mesa","Dallas","Daly City","Danbury","Davenport","Davidson County","Dayton","Daytona Beach","Deltona","Denton","Denver","Des Moines","Detroit","Downey","Duluth","Durham","El Monte","El Paso","Elizabeth","Elk Grove","Elkhart","Erie","Escondido","Eugene","Evansville","Fairfield","Fargo","Fayetteville","Fitchburg","Flint","Fontana","Fort Collins","Fort Lauderdale","Fort Smith","Fort Walton Beach","Fort Wayne","Fort Worth","Frederick","Fremont","Fresno","Fullerton","Gainesville","Garden Grove","Garland","Gastonia","Gilbert","Glendale","Grand Prairie","Grand Rapids","Grayslake","Green Bay","GreenBay","Greensboro","Greenville","Gulfport-Biloxi","Hagerstown","Hampton","Harlingen","Harrisburg","Hartford","Havre de Grace","Hayward","Hemet","Henderson","Hesperia","Hialeah","Hickory","High Point","Hollywood","Honolulu","Houma","Houston","Howell","Huntington","Huntington Beach","Huntsville","Independence","Indianapolis","Inglewood","Irvine","Irving","Jackson","Jacksonville","Jefferson","Jersey City","Johnson City","Joliet","Kailua","Kalamazoo","Kaneohe","Kansas City","Kennewick","Kenosha","Killeen","Kissimmee","Knoxville","Lacey","Lafayette","Lake Charles","Lakeland","Lakewood","Lancaster","Lansing","Laredo","Las Cruces","Las Vegas","Layton","Leominster","Lewisville","Lexington","Lincoln","Little Rock","Long Beach","Lorain","Los Angeles","Louisville","Lowell","Lubbock","Macon","Madison","Manchester","Marina","Marysville","McAllen","McHenry","Medford","Melbourne","Memphis","Merced","Mesa","Mesquite","Miami","Milwaukee","Minneapolis","Miramar","Mission Viejo","Mobile","Modesto","Monroe","Monterey","Montgomery","Moreno Valley","Murfreesboro","Murrieta","Muskegon","Myrtle Beach","Naperville","Naples","Nashua","Nashville","New Bedford","New Haven","New London","New Orleans","New York","New York City","Newark","Newburgh","Newport News","Norfolk","Normal","Norman","North Charleston","North Las Vegas","North Port","Norwalk","Norwich","Oakland","Ocala","Oceanside","Odessa","Ogden","Oklahoma City","Olathe","Olympia","Omaha","Ontario","Orange","Orem","Orlando","Overland Park","Oxnard","Palm Bay","Palm Springs","Palmdale","Panama City","Pasadena","Paterson","Pembroke Pines","Pensacola","Peoria","Philadelphia","Phoenix","Pittsburgh","Plano","Pomona","Pompano Beach","Port Arthur","Port Orange","Port Saint Lucie","Port St. Lucie","Portland","Portsmouth","Poughkeepsie","Providence","Provo","Pueblo","Punta Gorda","Racine","Raleigh","Rancho Cucamonga","Reading","Redding","Reno","Richland","Richmond","Richmond County","Riverside","Roanoke","Rochester","Rockford","Roseville","Round Lake Beach","Sacramento","Saginaw","Saint Louis","Saint Paul","Saint Petersburg","Salem","Salinas","Salt Lake City","San Antonio","San Bernardino","San Buenaventura","San Diego","San Francisco","San Jose","Santa Ana","Santa Barbara","Santa Clara","Santa Clarita","Santa Cruz","Santa Maria","Santa Rosa","Sarasota","Savannah","Scottsdale","Scranton","Seaside","Seattle","Sebastian","Shreveport","Simi Valley","Sioux City","Sioux Falls","South Bend","South Lyon","Spartanburg","Spokane","Springdale","Springfield","St. Louis","St. Paul","St. Petersburg","Stamford","Sterling Heights","Stockton","Sunnyvale","Syracuse","Tacoma","Tallahassee","Tampa","Temecula","Tempe","Thornton","Thousand Oaks","Toledo","Topeka","Torrance","Trenton","Tucson","Tulsa","Tuscaloosa","Tyler","Utica","Vallejo","Vancouver","Vero Beach","Victorville","Virginia Beach","Visalia","Waco","Warren","Washington","Waterbury","Waterloo","West Covina","West Valley City","Westminster","Wichita","Wilmington","Winston","Winter Haven","Worcester","Yakima","Yonkers","York","Youngstown"]
 /* HELPER FUNCTION TO RANDOMLY SELECT ROLE */
@@ -47,23 +48,69 @@ function pushTrainRow(named,destination,freq,ETA,minRemain){
 function checkNumber(n){
     return typeof parseInt(n) === Number;
 }
-$("#submitForm").on("click", function(event) {
-    event.preventDefault();
-    var trainID = Math.random().toString(36).substring(7);
-    var name = $("#trainName").text()
-    var dest = $("#dest").text()
-    var tTime = $("#train-time").text()
-    var freq = $("#freq").text()
-    console.log(name,dest,tTime,freq)
 
-    database.ref("/"+trainID).push({
-        ID : trainID,
-        name : name,
-        destination: dest,
-        timeInitial: tTime,
-        freq : freq
+function displayData(){
+
+    var dbref = database.ref("/trains/");
+
+    console.log(dbref.child("name"))
+    
+    var now = moment()
+    var first = moment(firstTime, "HH:mm").subtract(1, "years");
+    var diffTime = moment().diff(moment(first), "minutes");
+    var tRemainder = diffTime % tFrequency;
+    var tminus = tFrequency - tRemainder;
+    var next = moment().add(tminus, "minutes");
+
+    // pushTrainRow(nextTrain,next);
+}
+
+$(document).ready(function(){
+
+    // database.ref("/trains").on("child_added",function(snapshot) {
+    //     snapshot.forEach(function(row){
+    //         console.log(row.val);
+    //         console.log(row.child())
+    //             var _name = row.child().val().name;
+    //             var _dest = row.child().val().dest;
+    //             var _time = row.val().time;
+    //             var _freq = parseInt(row.val().freq);
+    //             // hard code in
+    //             pushTrainRow(_name,_dest,_freq,"0","0")
+        
+    //     });
+    displayData();
+
+ 
+
+    $("#submitForm").on("click", function(){
+        event.preventDefault();
+        // var trainID = Math.random().toString(36).substring(7);
+        var name = $("#trainName").val().trim()
+        var dest = $("#dest").val().trim()
+        var tTime = $("#train-time").val().trim()
+        var freq = $("#freq").val().trim()
+      
+        var newTrain = database.ref("/trains/train"+name).push();
+        newTrain.set({
+                name : name,
+                destination: dest,
+                timeInitial: tTime,
+                freq : freq
+            });
+        
+
+        
+
+        $("#trainName").val("")
+        $("#dest").val("")
+        $("#train-time").val("")
+        $("#freq").val("")
     });
+
 });
+
+
         // if(name){
         //     changeRed(name)
         // }
