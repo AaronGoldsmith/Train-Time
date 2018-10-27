786c083d7f2dfa26e3ff6f43d3b7fe40589387d8
Aaron Goldsmith <agoldsmith@alumni.scu.edu>
2018-10-26 20:52:34 -0700

    update backend config

--- assets/script.js
+++ assets/script.js
@@ -1,30 +1,17 @@
 
 // FIREBASE CONFIGURATION, INITIALIZATION
+var admin = require('firebase-admin');
+var serviceAccount = require('../train-scheduler-1b2a5-firebase-adminsdk-kc42l-c23669823f.json');
 
-  // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyC7Hc_Yh01FK4eZCV7h_ZMdA63xFWBqrfg",
-    authDomain: "train-scheduler-1b2a5.firebaseapp.com",
-    databaseURL: "https://train-scheduler-1b2a5.firebaseio.com",
-    projectId: "train-scheduler-1b2a5",
-    storageBucket: "train-scheduler-1b2a5.appspot.com",
-    messagingSenderId: "694734863457"
-  };
-  firebase.initializeApp(config);
+// Initialize Firebase
+var app = admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://train-scheduler-1b2a5.firebaseio.com/'
+  });
 
 // db ref
-var database = firebase.database();
-// var cities = ["Albuquerque","Alexandria","Allentown","Amarillo","Anaheim","Anchorage","Ann Arbor","Antioch","Apple Valley","Appleton","Arlington","Arvada","Asheville","Athens","Atlanta","Atlantic City","Augusta","Aurora","Austin","Bakersfield","Baltimore","Barnstable","Baton Rouge","Beaumont","Bel Air","Bellevue","Berkeley","Bethlehem","Billings","Birmingham","Bloomington","Boise","Boise City","Bonita Springs","Boston","Boulder","Bradenton","Bremerton","Bridgeport","Brighton","Brownsville","Bryan","Buffalo","Burbank","Burlington","Cambridge","Canton","Cape Coral","Carrollton","Cary","Cathedral City","Cedar Rapids","Champaign","Chandler","Charleston","Charlotte","Chattanooga","Chesapeake","Chicago","Chula Vista","Cincinnati","Clarke County","Clarksville","Clearwater","Cleveland","College Station","Colorado Springs","Columbia","Columbus","Concord","Coral Springs","Corona","Corpus Christi","Costa Mesa","Dallas","Daly City","Danbury","Davenport","Davidson County","Dayton","Daytona Beach","Deltona","Denton","Denver","Des Moines","Detroit","Downey","Duluth","Durham","El Monte","El Paso","Elizabeth","Elk Grove","Elkhart","Erie","Escondido","Eugene","Evansville","Fairfield","Fargo","Fayetteville","Fitchburg","Flint","Fontana","Fort Collins","Fort Lauderdale","Fort Smith","Fort Walton Beach","Fort Wayne","Fort Worth","Frederick","Fremont","Fresno","Fullerton","Gainesville","Garden Grove","Garland","Gastonia","Gilbert","Glendale","Grand Prairie","Grand Rapids","Grayslake","Green Bay","GreenBay","Greensboro","Greenville","Gulfport-Biloxi","Hagerstown","Hampton","Harlingen","Harrisburg","Hartford","Havre de Grace","Hayward","Hemet","Henderson","Hesperia","Hialeah","Hickory","High Point","Hollywood","Honolulu","Houma","Houston","Howell","Huntington","Huntington Beach","Huntsville","Independence","Indianapolis","Inglewood","Irvine","Irving","Jackson","Jacksonville","Jefferson","Jersey City","Johnson City","Joliet","Kailua","Kalamazoo","Kaneohe","Kansas City","Kennewick","Kenosha","Killeen","Kissimmee","Knoxville","Lacey","Lafayette","Lake Charles","Lakeland","Lakewood","Lancaster","Lansing","Laredo","Las Cruces","Las Vegas","Layton","Leominster","Lewisville","Lexington","Lincoln","Little Rock","Long Beach","Lorain","Los Angeles","Louisville","Lowell","Lubbock","Macon","Madison","Manchester","Marina","Marysville","McAllen","McHenry","Medford","Melbourne","Memphis","Merced","Mesa","Mesquite","Miami","Milwaukee","Minneapolis","Miramar","Mission Viejo","Mobile","Modesto","Monroe","Monterey","Montgomery","Moreno Valley","Murfreesboro","Murrieta","Muskegon","Myrtle Beach","Naperville","Naples","Nashua","Nashville","New Bedford","New Haven","New London","New Orleans","New York","New York City","Newark","Newburgh","Newport News","Norfolk","Normal","Norman","North Charleston","North Las Vegas","North Port","Norwalk","Norwich","Oakland","Ocala","Oceanside","Odessa","Ogden","Oklahoma City","Olathe","Olympia","Omaha","Ontario","Orange","Orem","Orlando","Overland Park","Oxnard","Palm Bay","Palm Springs","Palmdale","Panama City","Pasadena","Paterson","Pembroke Pines","Pensacola","Peoria","Philadelphia","Phoenix","Pittsburgh","Plano","Pomona","Pompano Beach","Port Arthur","Port Orange","Port Saint Lucie","Port St. Lucie","Portland","Portsmouth","Poughkeepsie","Providence","Provo","Pueblo","Punta Gorda","Racine","Raleigh","Rancho Cucamonga","Reading","Redding","Reno","Richland","Richmond","Richmond County","Riverside","Roanoke","Rochester","Rockford","Roseville","Round Lake Beach","Sacramento","Saginaw","Saint Louis","Saint Paul","Saint Petersburg","Salem","Salinas","Salt Lake City","San Antonio","San Bernardino","San Buenaventura","San Diego","San Francisco","San Jose","Santa Ana","Santa Barbara","Santa Clara","Santa Clarita","Santa Cruz","Santa Maria","Santa Rosa","Sarasota","Savannah","Scottsdale","Scranton","Seaside","Seattle","Sebastian","Shreveport","Simi Valley","Sioux City","Sioux Falls","South Bend","South Lyon","Spartanburg","Spokane","Springdale","Springfield","St. Louis","St. Paul","St. Petersburg","Stamford","Sterling Heights","Stockton","Sunnyvale","Syracuse","Tacoma","Tallahassee","Tampa","Temecula","Tempe","Thornton","Thousand Oaks","Toledo","Topeka","Torrance","Trenton","Tucson","Tulsa","Tuscaloosa","Tyler","Utica","Vallejo","Vancouver","Vero Beach","Victorville","Virginia Beach","Visalia","Waco","Warren","Washington","Waterbury","Waterloo","West Covina","West Valley City","Westminster","Wichita","Wilmington","Winston","Winter Haven","Worcester","Yakima","Yonkers","York","Youngstown"]
-/* HELPER FUNCTION TO RANDOMLY SELECT ROLE */
+var database = app.database();
 
-
-/* ADD CITIES ONE TIME TO BACKEND */
-function addCities(){
-    for(var i = 0;i<cities.length;i++){
-        var cur = "city"+i;
-        database.ref("/cities/").push(cities[i]);
-    }
-}
 
 function changeRed(element){
     element.css("background","red")
@@ -61,12 +48,6 @@ $(document).ready(function(){
                 freq : freq
             };
         database.ref().push(newTrain);
-        console.log(newTrain.name)
-        console.log(newTrain.destination)
-        console.log(newTrain.timeInitial)
-        console.log(newTrain.freq)
-
-        
 
         $("#trainName").val("")
         $("#dest").val("")
